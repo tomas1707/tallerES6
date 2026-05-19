b92702002b9b210f721679e5ab7c0df5e65cfedb
Tomas Gonzalez Alvarado <tomas.gonzalez@smartin.tecnm.mx>
2026-05-18 21:12:47 -0600

    Ejemplo2

--- public/js/ejemplo1.js
+++ public/js/ejemplo1.js
@@ -2,3 +2,11 @@ function saludo (){
     console.log ("Hola mundo");
     //alert("Hola Mundo");
 }
+
+function suma(a,b){
+    var suma;
+    suma= a + b;
+    console.log("La suma es "+ suma);
+}
+
+
---
+++ public/js/ejemplo2.js
@@ -0,0 +1,12 @@
+console.log ("Hola Mundo");
+
+function Suma (a,b){
+    return a+b;
+}
+
+let a;
+var b;
+a=10;
+b=20;
+let suma=Suma(a,b);
+ console.log("La suma es "+ suma);
